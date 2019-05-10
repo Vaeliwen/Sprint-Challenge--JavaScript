faa86ea5406fd221eaec3afc358005cd43d8b720
Vaeliwen <zaman@imbriaco.com>
2019-05-10 12:02:49 -0400

    Completed objects-arrays.js.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -140,7 +140,10 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(function(item) {
+  lower_case = item.animal_name;
+  return lower_case.toLowerCase()
+});
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -148,7 +151,7 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter((item) => {return item.population < 5});
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -156,7 +159,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((total, amount) => {
+  return total += amount.population;
+}, 0)
 console.log(populationTotal);
 
 
